818dccefeb145590022e1fa982fd46545e8c4ca6
userfin <klappauser@gmail.com>
2023-05-07 19:53:50 +0300

    Mainpage styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,34 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
+
+import type { Metadata } from "next"
+import LargeHeading from '@/components/ui/LargeHeading'
+import Paragraph from '@/components/ui/Paragraph'
+import Link from 'next/link'
+
+export const metadata: Metadata = {
+  title: 'Similarity API | Home',
+  description: 'Free & open-source text similarity API'
+}
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return (
-    <main className='bg-red-500'>
-      
-    </main>
-  )
+  return <div className='relative h-screen flex items-center justify-center overflow-x-hidden'>
+    <div className='container pt-32 max-w-7xl mx-auto w-full h-full'>
+      <div className='h-full gap-6 flex flex-col justify-start lg:justify-center items-center lg:items-start'>
+        <LargeHeading size='lg' className='three-d text-black dark:text-light-gold'>
+          Easily determine <br /> text similarity.
+        </LargeHeading>
+
+        <Paragraph className='max-w-xl lg:text-left'>
+          With the text similarity API, you can easily determine the
+          similarity between 2 pieces of text with a free{' '}
+          <Link href='/login' className='underline underline-offset-2 text-black dark:text-light-gold'>
+            API key
+          </Link>
+          .
+        </Paragraph>
+      </div>
+    </div>
+  </div>
 }
